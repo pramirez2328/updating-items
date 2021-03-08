d1b0848aa1c33520b1bc45306a09f20f6d8902c8
Pedro Ramirez <prlara2000@gmail.com>
2021-03-07 23:42:45 -0600

    adding style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import {movies} from './services/fakeMovieService';
+
+ const styles = {
+   width: "80%",
+   margin: "auto",
+ };
+
+class App extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			numberOfMovies: movies.length
+		}
+		this.handleUpdatingTheNumber = this.handleUpdatingTheNumber.bind(this);
+		this.handleDelete = this.handleDelete.bind(this);
+	};
+
+	handleDelete () {
+    return this.handleUpdatingTheNumber();
+  };
+
+	handleUpdatingTheNumber () {
+		this.setState({
+			numberOfMovies: this.state.numberOfMovies - 1
+		})
+	}
+
+	render(){
+    return (
+      <div style={styles}>
+        <h1 style={{ textAlign: "center", margin: "2rem" }}>
+          Showing {this.state.numberOfMovies} movies in the database
+        </h1>
+        <table className="table table-striped">
+          <thead style={{ textTransform: "uppercase", border: "solid 2px red" }}>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col">Update</th>
+            </tr>
+          </thead>
+          <tbody>{movies.map((movie) => {
+                    return <tr key={movie._id}>
+                             <th scope="row">{movie.title}</th>
+                               <td>{movie.genre.name}</td>
+                               <td>{movie.numberInStock}</td>
+                               <td>{movie.dailyRentalRate}</td>
+                               <td>
+				                          <button className="hover" onClick={this.handleDelete}>
+                                    Delete
+                                </button>
+				                       </td>
+                            </tr>
+        })}</tbody>
+        </table>
+      </div>
+    );
+  }
+
+	}
+
 
 export default App;
